1f2ff9fbc904c41e9e85cb152874b2f71be7456d
Cameron Button and Dawn Mott <cameron@>
2018-04-18 10:33:50 -0700

    add item class and test item type

--- spec/rpg-spec.js
+++ spec/rpg-spec.js
@@ -1,5 +1,10 @@
 import {Character} from './../src/rpg.js';
-let player = new Character('Dawn', 'mage');
+import {Gift} from './../src/rpg.js';
+
+let player = new Character('Dawn', 'mage',);
+let wand = new Gift('wand');
+let sword = new Gift('sword');
+let lute = new Gift('lute');
 
 describe('character', function(){
   it('should test if the character has a name', function(){
@@ -7,5 +12,11 @@ describe('character', function(){
   });
   it('should test for the character type', function(){
     expect(player.type).toEqual('mage');
+  });
+});
+
+describe('gift', function(){
+  it('should test character type and gift accordingly', function(){
+    expect(wand.item).toEqual('wand');
   });
 });
--- src/rpg.js
+++ src/rpg.js
@@ -4,3 +4,21 @@ export class Character {
     this.type = type;
   }
 }
+
+export class Gift {
+  constructor (item){
+    this.item = item;
+  // }
+  // if (player.type === mage){
+  //   let gift = wand;
+  // } else if {
+  //   (player.type === warrior) {
+  //     let gift = sword;
+  //   }
+  //   else if {
+  //     (player.type === bard) {
+  //       let gift = lute;
+  //     }
+  //   }
+  }
+}
