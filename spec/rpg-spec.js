b039be350956842344e35145bcb0b63b8e0dc3c8
Cameron Button and Dawn Mott <dawn@>
2018-04-18 13:26:42 -0700

    test damage system

--- spec/rpg-spec.js
+++ spec/rpg-spec.js
@@ -1,7 +1,10 @@
 import {Character} from './../src/rpg.js';
 import {Gift} from './../src/rpg.js';
+// import {Enemy} from './../src/rpg.js';
+
 
 let player = new Character('Dawn', 'mage',);
+let enemy = new Character('bad guy', 'Garudo');
 let wand = new Gift('wand');
 let sword = new Gift('sword');
 let lute = new Gift('lute');
@@ -12,11 +15,20 @@ describe('character', function(){
   });
   it('should test for the character type', function(){
     expect(player.type).toEqual('mage');
+    player.checkType();
+    expect(player.int).toEqual(16);
   });
 });
 
 describe('gift', function(){
   it('should test character type and gift accordingly', function(){
     expect(wand.item).toEqual('wand');
+  });
+});
+
+describe('enemyAttack', function(){
+  it('should take points away from health per damage dealt', function() {
+    player.health -= enemy.damage;
+    expect(player.health).toEqual(6);
   });
 });
--- src/rpg.js
+++ src/rpg.js
@@ -2,7 +2,31 @@ export class Character {
   constructor (name, type){
     this.name = name;
     this.type = type;
+    this.health = 10;
+    this.str = 10;
+    this.isma = 10;
+    this.int = 10;
+    this.damage = 5;
   }
+
+  checkType(){
+    if(this.type === 'mage'){
+      this.health ++;
+      this.int += 6;
+      this.isma -= 4;
+    } else if (this.type === 'warrior'){
+      this.health += 5;
+      this.int -= 4;
+      this.str ++;
+    } else if (this.type === 'bard'){
+      this.isma += 8;
+      this.str -= 4;
+      this.health -=3;
+    }
+  }
+
+
+
 }
 
 export class Gift {
@@ -22,3 +46,13 @@ export class Gift {
   //   }
   }
 }
+export function enemy(name) {
+  this.name = name;
+  this.health = 20;
+  this.damage = 5;
+}
+
+enemy.prototype.enemyAttack = function(){
+  let damage = 5;
+  return damage;
+};
